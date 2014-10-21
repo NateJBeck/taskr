8bacf6309c257ac15f3fbe8fbc8125cb7efa6ee1
Nathan Beck <nathan.beck10@gmail.com>
2014-10-21 14:40:28 -0400

    Users can complete tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,12 @@
 class TasksController < ApplicationController
   def index
-    @tasks = current_user.tasks
+    @tasks = current_user.tasks.incomplete
     @task = Task.new
   end
 
   def create
     @task = Task.new(task_params)
-    @tasks = current_user.tasks
+    @tasks = current_user.tasks.incomplete
 
     if @task.save
       current_user.tasks.create(task_params)
@@ -16,10 +16,17 @@ class TasksController < ApplicationController
     end
   end
 
+  def update
+    task = current_user.tasks.find(params[:id])
+    task.update(task_params)
+
+    redirect_to tasks_path
+  end
+
   private
 
   def task_params
-    params.require(:task).permit(:title, :description)
+    params.require(:task).permit(:title, :description, :completed)
   end
 
 end
