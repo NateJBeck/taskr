6a6671ff2a46c7453737a1580c6390d9a9b69e9b
Nathan Beck <nathan.beck10@gmail.com>
2014-10-23 15:25:00 -0400

    Clean up & structure title editing code

--- app/assets/javascripts/edit-task.js
+++ app/assets/javascripts/edit-task.js
@@ -1,33 +1,29 @@
 $(function() {
-  var editTitle = function() {
-    $(this).attr("contenteditable", true)
-   return false;
-  };
-
-  var changeTitle = function(event) {
-    if ( event.keyCode === 13 ) {
-      $(this).attr("contenteditable", false)
-      $(this).highlight();
-
-      var currentElement = $(this)
-
-      var id = currentElement.parent().data("id")
-      var object = { task: { title: currentElement.text() } }
-
-      var theElement = $(this);
-      var taskData = theElement.serialize();
-
-      var conversation = $.ajax({
-        url: "/tasks/" + id,
-        type: "PATCH",
-        data: object
-      })
-
-      console.log("enter")
-    }
-  };
-
   $("body").on("click", ".title", editTitle);
   $("body").on("keydown", ".title", changeTitle);
 });
+
+var editTitle = function() {
+  $(this).attr("contenteditable", true)
+  return false;
+};
+
+var changeTitle = function(event) {
+  if ( event.keyCode === 13 ) {
+    $(this).attr("contenteditable", false)
+    $(this).highlight();
+
+    var currentElement = $(this)
+
+    var id = currentElement.parents(".task").data("id")
+    var object = { task: { title: currentElement.text() } }
+
+    var conversation = $.ajax({
+      url: "/tasks/" + id,
+      type: "PATCH",
+      data: object
+    })
+  };
+};
+
 
